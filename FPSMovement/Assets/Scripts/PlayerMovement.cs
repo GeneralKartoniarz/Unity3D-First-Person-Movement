449966e5247d4aab6e07d83087ab3c208580f097
Kartoniarz <hardkanterpl@gmail.com>
2023-06-06 20:10:04 +0200

    Add jumping

--- FPSMovement/Assets/Scripts/PlayerMovement.cs
+++ FPSMovement/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Key bindings")]
+    public static KeyCode jumpKey = KeyCode.Space;
+    public static KeyCode crouchKey = KeyCode.LeftShift;
+    [Header("Moving")]
     [SerializeField] float walkingSpeed;
     [SerializeField] float runningSpeed;
+    [Header("Jumping")]
     [SerializeField] float jumpingForce;
+    [SerializeField] float jumpingCooldown;
+    [SerializeField] float airMultiplier;
+    [SerializeField] bool canJump;
+    [Header("Physics")]
     [SerializeField] float gravityForce;
     [SerializeField] bool canMove;
     [SerializeField] float groundDrag;
@@ -27,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         //assigning variables
+        canJump = true;
         playerHeight = 2f;
         player = GetComponent<Transform>().transform;
         playerRb = GetComponent<Rigidbody>();
@@ -37,19 +47,40 @@ public class PlayerMovement : MonoBehaviour
         //getting inputs
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
+        //jumping
+        if(Input.GetKey(jumpKey) && isGrounded && canJump)
+        {
+            canJump = false;
+            Jump();
+            Invoke("CanJumpReset", jumpingCooldown);
+        }
         //checking if is grounded
         isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.1f, ground);
         //adding drag (removing sliding)
         if (isGrounded) playerRb.drag = groundDrag;
         else playerRb.drag = 0;
-
+        //removing acceleration (speed control)
+        Vector3 actualVelocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z);
+        if(actualVelocity.magnitude > walkingSpeed)
+        {
+            Vector3 limitedVelocity = actualVelocity.normalized * walkingSpeed;
+            playerRb.velocity = new Vector3(limitedVelocity.x, playerRb.velocity.y, limitedVelocity.z);
+        }
         MovePlayer();
     }
-    
     //getting direction and adding force
     private void MovePlayer()
     {
         moveDirection = player.forward * yInput + player.right * xInput;
         playerRb.AddForce(moveDirection.normalized * walkingSpeed, ForceMode.Force);
+    }
+    private void Jump()
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z);
+        playerRb.AddForce(transform.up * jumpingForce, ForceMode.Impulse);
+    }
+    private void CanJumpReset()
+    {
+        canJump = true;
     }
 }
