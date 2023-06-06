95fc3f2ce18e083f149b916cbbd284cc1bc96859
Kartoniarz <hardkanterpl@gmail.com>
2023-06-06 13:44:53 +0200

    Camera Movement

---
+++ FPSMovement/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float mouseSens = 20f;
+    [SerializeField] float angleYmax = 20f;
+    private float yRot;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        //getting inputs
+        float xInput = Input.GetAxis("Mouse X") * mouseSens;
+        float yInput = Input.GetAxis("Mouse Y") * mouseSens;
+        //rotating camera on Y axis and adding max angle
+        yRot -= yInput;
+        yRot = Mathf.Clamp(yRot, -angleYmax, angleYmax);
+        mainCamera.transform.localEulerAngles = Vector3.right * yRot;
+        //rotating player object
+        mainCamera.transform.parent.Rotate(Vector3.up * xInput);
+    }
+}
