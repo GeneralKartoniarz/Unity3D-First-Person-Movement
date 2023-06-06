0fa54527bd86688bc28863ce33138a39da30a12b
Kartoniarz <hardkanterpl@gmail.com>
2023-06-06 19:47:34 +0200

    Removing sliding
    Adding layer "ground" and adding drag to remove sliding on surface

--- FPSMovement/Assets/Scripts/PlayerMovement.cs
+++ FPSMovement/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpingForce;
     [SerializeField] float gravityForce;
     [SerializeField] bool canMove;
+    [SerializeField] float groundDrag;
+    [SerializeField] LayerMask ground;
 
     private float xInput;
     private float yInput;
     private bool isRunning;
+    private bool isGrounded;
+    private float playerHeight;
 
     private Rigidbody playerRb;
     private Vector3 moveDirection;
     private Transform player;
+
+
     
     void Start()
     {
         //assigning variables
+        playerHeight = 2f;
         player = GetComponent<Transform>().transform;
         playerRb = GetComponent<Rigidbody>();
         playerRb.freezeRotation = true;
@@ -30,6 +37,12 @@ public class PlayerMovement : MonoBehaviour
         //getting inputs
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
+        //checking if is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.1f, ground);
+        //adding drag (removing sliding)
+        if (isGrounded) playerRb.drag = groundDrag;
+        else playerRb.drag = 0;
+
         MovePlayer();
     }
     
